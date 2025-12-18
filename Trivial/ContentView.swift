de6015ca5c95b49ee943b6729e476055c7157fb9
Andrew <vanillamoon210@gmail.com>
2025-12-18 17:31:59 -0500

    Initial Commit

---
+++ Trivial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trivial
+//
+//  Created by Andrew Allen on 12/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trivial/TrivialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivialApp.swift
+//  Trivial
+//
+//  Created by Andrew Allen on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TrivialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
